c1c4ac79c4a6024f65b6879f245dd3ae2e1e5959
hmitgang <hmitgang@gmail.com>
2024-01-18 16:54:00 -0800

    FE: added faceblurring

--- frontend/src/components/submit/SubmitRecord.jsx
+++ frontend/src/components/submit/SubmitRecord.jsx
@@ -4,7 +4,7 @@ import SubmitRecordControls from './SubmitRecordControls';
 
 const SubmitRecord = ({assignmentData, confirmSubmission}) => {
     
-    const {timeLimitMinutes, description, allowFaceBlur} = assignmentData;
+    const {timeLimitMinutes, description, allowFaceBlur, name} = assignmentData;
 
     const [secondsRemaining, setSecondsRemaining] = useState(timeLimitMinutes*60);
     const [blurface, setBlurface] = useState(false);
@@ -13,46 +13,55 @@ const SubmitRecord = ({assignmentData, confirmSubmission}) => {
     const [hasRecorded, setHasRecorded] = useState(false);
 
     return (
-        <div class="grid grid-cols-1 m-5 md:grid-cols-3">
-
-            {/* TODO: set max height on video to not push controls off the page */}
-            <div class="w-full h-full border order-1 md:col-span-2">
-            {
-                hasRecorded && !recording ?
-                <video />
-                :
-                <Video  blurface={blurface} />
-            }
-            </div>
-            
-
-            <div class="order-3 md:order-2 md:mx-2">
-                {description}
+        <div className="md:h-dvh">
+            <div className="m-5 mb-0">
+                <span className="text-black text-6xl self-center max-md:max-w-full max-md:text-4xl">
+                    {name}
+                </span>
+                <div className="bg-black self-stretch mt-1 shrink-0 h-0.5 max-md:max-w-full" />
             </div>
 
-            {/* Controls */}
+            <div className="grid grid-cols-1 m-5 mt-2 md:grid-cols-3">
 
-            {
-                !recording && hasRecorded ?
-                <div class="order-4 md:order-3">
-                    <button class="button text-white text-center justify-center text-l font-black bg-green-500 hover:bg-green-400 self-center px-5 py-2 text-nowrap rounded-md"
-                        onClick={() => {
-                            setRecording(true);
-                        }}
-                    >Re-record</button>
-                    <button class="button text-white text-center justify-center text-l font-black bg-indigo-500 hover:bg-indigo-400 self-center px-5 py-2 text-nowrap rounded-md"
-                        onClick={() => {
-                            confirmSubmission();
-                        }}
-                    >Submit</button>
+                {/* TODO: set max height on video to not push controls off the page */}
+                <div className="w-full border order-1 md:col-span-2">
+                {
+                    hasRecorded && !recording ?
+                    <video />
+                    :
+                    <Video  blurface={blurface} />
+                }
                 </div>
-                :
-                <SubmitRecordControls 
-                recording={recording} setRecording={setRecording}  setHasRecorded={setHasRecorded}
-                secondsRemaining={secondsRemaining} allowFaceBlur={allowFaceBlur} blurface={blurface} setBlurface={setBlurface}
-                />
-            }
-            
+                
+
+                <div className="order-3 md:order-2 md:mx-2">
+                    {description}
+                </div>
+
+                {/* Controls */}
+
+                {
+                    !recording && hasRecorded ?
+                    <div className="order-4 md:order-3">
+                        <button className="button text-white text-center justify-center text-l font-black bg-green-500 hover:bg-green-400 self-center px-5 py-2 text-nowrap rounded-md"
+                            onClick={() => {
+                                setRecording(true);
+                            }}
+                        >Re-record</button>
+                        <button className="button text-white text-center justify-center text-l font-black bg-indigo-500 hover:bg-indigo-400 self-center px-5 py-2 text-nowrap rounded-md"
+                            onClick={() => {
+                                confirmSubmission();
+                            }}
+                        >Submit</button>
+                    </div>
+                    :
+                    <SubmitRecordControls 
+                    recording={recording} setRecording={setRecording}  setHasRecorded={setHasRecorded}
+                    secondsRemaining={secondsRemaining} allowFaceBlur={allowFaceBlur} blurface={blurface} setBlurface={setBlurface}
+                    />
+                }
+                
+            </div>
         </div>
     );
 };
--- frontend/src/components/submit/Video.jsx
+++ frontend/src/components/submit/Video.jsx
@@ -1,8 +1,15 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+
+import * as faceapi from 'face-api.js';
+
 
 const Video = ({class: cls, blurface}) => {
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
+    const requestRef = useRef()
+
+    const [modelsLoaded, setModelsLoaded] = useState(false);
+    const [captureVideo, setCaptureVideo] = useState(false);
 
     
 
@@ -11,7 +18,19 @@ const Video = ({class: cls, blurface}) => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
-        console.log(blurface)
+        const loadModels = async () => {
+            const MODEL_URL = "https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights";
+        
+
+            Promise.all([
+                faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+                faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+                faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+                faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+            ]).then(() => console.log("Loaded models")).then(setModelsLoaded(true));
+        }
+        loadModels();
+
 
         // TODO: Include audio
         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
@@ -42,28 +61,43 @@ const Video = ({class: cls, blurface}) => {
         const video = videoRef.current;
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
-        
-        const drawFrame = () => {
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-            if (blurface) {
-                // Just to show we can draw on the canvas
-                context.beginPath();
-                context.arc(50, 50, 50, 0, 2 * Math.PI);
-                context.fillStyle = 'red';
-                context.fill();
+        const drawFrame = async () => {
+            
+            // 
+
+            const detections = (modelsLoaded && blurface ?
+                await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+                : []
+                );
+
+            // context.clearRect(0, 0, canvas.width, canvas.height)
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            for (const detection of detections) {
+                context.filter = "blur(10px)"
+                context.drawImage(video, detection.box.x, detection.box.y, detection.box.width, detection.box.height,
+                    detection.box.x, detection.box.y, detection.box.width, detection.box.height);
+                context.filter = "none"
             }
 
-            requestAnimationFrame(drawFrame);
+            // requestRef.current = requestAnimationFrame(drawFrame);
         };
 
-        drawFrame();
-    }, [blurface]);
+        // TODO: fix request animation frames
+        requestRef.current = setInterval(drawFrame, 1000/30)
+
+        // requestRef.current = requestAnimationFrame(drawFrame);
+
+        return () => {
+            // cancelAnimationFrame(requestRef.current);
+            clearInterval(requestRef.current)
+        }
+    }, [blurface, modelsLoaded]);
 
     return (
-        <div class={cls}>
-            <canvas class="w-full" ref={canvasRef} />
-            <video class="hidden" playsInline muted ref={videoRef} />
+        <div className={cls}>
+            <canvas className="w-full" ref={canvasRef} />
+            <video className="hidden" playsInline autoPlay muted ref={videoRef} />
         </div>
     );
 };
