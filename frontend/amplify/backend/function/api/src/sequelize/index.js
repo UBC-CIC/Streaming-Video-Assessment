7fc94a05c1ceac39d81c38ff23ad0c538e7cdefc
hmitgang <hmitgang@gmail.com>
2024-02-10 14:27:09 -0800

    Define Sequelize models

---
+++ frontend/amplify/backend/function/api/src/sequelize/index.js
@@ -0,0 +1,33 @@
+const { Sequelize } = require("sequelize");
+const { applyExtraSetup } = require("./extra-setup");
+
+// In a real app, you should keep the database connection URL as an environment variable.
+// But for this example, we will just use a local SQLite database.
+// const sequelize = new Sequelize(process.env.DB_CONNECTION_URL);
+const sequelize = new Sequelize("sqlite::memory:", {
+  //   dialect: "sqlite",
+  //   storage: "sqlite-example-database/example-db.sqlite",
+  //   logQueryParameters: true,
+  //   benchmark: true,
+});
+
+const modelDefiners = [
+  require("./models/assessment.model"),
+  require("./models/folder.model"),
+  require("./models/uploader.model"),
+  require("./models/uploaderGroup.model"),
+  require("./models/uploadRequest.model"),
+  require("./models/user.model"),
+  require("./models/video.model"),
+];
+
+// We define all models according to their files.
+for (const modelDefiner of modelDefiners) {
+  modelDefiner(sequelize);
+}
+
+// We execute any extra setup after the models are defined, such as adding associations.
+applyExtraSetup(sequelize);
+
+// We export the sequelize connection instance to be used around our app.
+module.exports = sequelize;
---
+++ frontend/amplify/backend/function/api/src/sequelize/models/uploaderGroup.model.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+
+// We export a function that defines the model.
+// This function will automatically receive as parameter the Sequelize connection object.
+module.exports = (sequelize) => {
+  sequelize.define(
+    "uploaderGroup",
+    {
+      // The following specification of the 'id' attribute could be omitted
+      // since it is the default.
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      folderId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      indexes: [
+        {
+          unique: true,
+          fields: ["name", "folderId"],
+        },
+      ],
+    },
+  );
+};
