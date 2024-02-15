9475ac7e646c52785c66fbbd6ede554471351ba2
hmitgang <hmitgang@gmail.com>
2024-02-14 18:43:42 -0800

    Add uploaders to a group

--- frontend/amplify/backend/function/api/src/sequelize/create-dummy-data.js
+++ frontend/amplify/backend/function/api/src/sequelize/create-dummy-data.js
@@ -6,12 +6,12 @@
 
 const sequelize = require(".");
 
-const { user, folder } = sequelize.models;
+const { user, folder, uploaderGroup, uploader } = sequelize.models;
 
 async function create() {
   const user1 = await user.create(
     {
-      email: "hmitgang+4@student.ubc.ca",
+      email: "hmitgang+5@student.ubc.ca",
       isPlatformManager: true,
       isAssessmentCreator: true,
       folders: [
@@ -34,6 +34,18 @@ async function create() {
     ownerId: user1.id,
     parentId: root.id,
   });
+
+  const spanish1students = await uploaderGroup.create({
+    name: "Students",
+    folderId: spanish1.id,
+  });
+
+  const uploader1 = await uploader.create({
+    email: "hmitgang@student.ubc.ca",
+    name: "Harrison Mitgang",
+  });
+
+  await spanish1students.addUploader(uploader1);
 
   const spanish2 = await folder.create({
     name: "Spanish 2",
