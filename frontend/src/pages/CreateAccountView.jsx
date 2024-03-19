b9374a07a43ef91fdf69cfd30ae6e0890b618e83
Eran Deutsch <erand50@outlook.com>
2024-03-18 23:09:14 -0700

    no more CSS!

--- frontend/src/pages/CreateAccountView.jsx
+++ frontend/src/pages/CreateAccountView.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./styles/Login.css";
 import { handleSignUp } from "../helpers/authenticationHandler";
 
 function CreateAccountView() {
@@ -33,31 +32,29 @@ function CreateAccountView() {
   const passwordRules = password.length > 5;
   const passwordExists = password.length > 0;
 
+  var confirmButtonColor = ((passwordsMatch && passwordRules) ? "#6a7dc2" : "#b1b1b1");
+
   return (
-    <div className="flex flex-col justify-center items-center max-md:px-5"
-    style={{
-      height: "90vh",
-      // marginTop: "10%"
-    }}>
-      <div className="flex w-[344px] max-w-full flex-col items-stretch mt-52 mb-36 max-md:my-10">
+    <div className="flex flex-col justify-center items-center max-md:px-5 h-dvh">
+      <div className="flex w-[344px] max-w-full flex-col items-stretch mt-25 max-md:my-10">
         <div className="justify-center text-center text-indigo-500 text-2xl font-black tracking-wider self-stretch">
           CREATE ACCOUNT
         </div>
         <input
-          className="input-field mt-9 max-md:pr-5"
+          className="whitespace-nowra w-full justify-center pl-[17px] pr-[60px] py-[21px] border-2 border-solid border-black align-items: start font: 400 15px Roboto, sans-serif mt-9 max-md:pr-5"
           name="name"
           placeholder="Name"
           onInput={(e) => setName(e.target.value)}
         />
         <input
-          className="input-field mt-9 max-md:pr-5"
+          className="whitespace-nowra w-full justify-center pl-[17px] pr-[60px] py-[21px] border-2 border-solid border-black align-items: start font: 400 15px Roboto, sans-serif mt-9 max-md:pr-5"
           name="email"
           placeholder="Email"
           onInput={(e) => setEmail(e.target.value)}
           value={email}
         />
         <input
-          className="input-field mt-9 max-md:pr-5"
+          className="whitespace-nowra w-full justify-center pl-[17px] pr-[60px] py-[21px] border-2 border-solid border-black align-items: start font: 400 15px Roboto, sans-serif mt-9 max-md:pr-5"
           style={{border:"2px solid #000", padding: "17.5px 60px 17.5px 17px"}}
           name="password"
           placeholder="Password"
@@ -65,7 +62,7 @@ function CreateAccountView() {
           onInput={(e) => setPassword(e.target.value)}
         />
         <input
-          className="input-field mt-9 max-md:pr-5"
+          className="whitespace-nowra w-full justify-center pl-[17px] pr-[60px] py-[21px] border-2 border-solid border-black align-items: start font: 400 15px Roboto, sans-serif mt-9 max-md:pr-5"
           style={{border:"2px solid #000", padding: "17.5px 60px 17.5px 17px"}}
           name="confirm-password"
           placeholder="Confirm Password"
@@ -76,10 +73,10 @@ function CreateAccountView() {
         <div className="justify-center text-center text-red-500 tracking-wider self-stretch"
         style={{padding: "20px 0px 0px 0px"}}>Password must be 6 characters or longer</div>}
         <div className="flex items-stretch justify-between gap-5 mt-12 max-md:mt-10">
-          <button className={passwordsMatch && passwordRules ? "create-button" : "create-button-disabled"} onClick={onCreateAccountClick} disabled={!passwordsMatch}>
+          <button className={"text-white text-center tracking-[0.52px] whitespace-nowrap bg-["+ confirmButtonColor +"] justify-center px-[13px] py-[27px] rounded-md text-sm font-bold"} onClick={onCreateAccountClick} disabled={!passwordsMatch}>
             CREATE ACCOUNT
           </button>
-          <button className="back-to-login-button" onClick={onBackToLoginClick}>
+          <button className="text-black tracking-[0.52px] whitespace-nowrap bg-[#c9c9c9] w-6/12 justify-center self-center px-5 py-[27px] rounded-md border-2 border-solid border-[#c9c9c9] text-sm font-bold" onClick={onBackToLoginClick}>
             BACK
           </button>
         </div>
--- frontend/src/pages/LoginView.jsx
+++ frontend/src/pages/LoginView.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./styles/Login.css";
 import { handleSignIn, handleSignOut, checkUserSignInStatus } from "../helpers/authenticationHandler";
 
 function LoginView() {
@@ -51,25 +50,21 @@ function LoginView() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center" 
-      style={{
-        height: "80vh",
-        marginTop: "10%"
-      }}>
-      <div className="flex w-[350px] max-w-full flex-col items-stretch">
+    <div className="flex flex-col justify-center items-center h-dvh">
+      <div className="flex w-[350px] max-w-full flex-col items-stretch mt-25 max-md:my-10">
         <div className="justify-center text-indigo-500 text-center text-6xl font-black tracking-[2.56px] max-md:text-4xl">
           DROPZONE
         </div>
         <input
-          className="input-field max-md:mr-2 max-md:pr-5"
+          className="whitespace-nowra w-full justify-center pl-[17px] pr-[60px] py-[21px] border-2 border-solid border-black align-items: start font: 400 15px Roboto, sans-serif mt-9 max-md:pr-5"
           name="email"
           placeholder="Email"
           onInput={(e) => setEmail(e.target.value)}
           value={email}
         />
         <input
-          className="input-field max-md:mr-2 max-md:pr-5"
-          style={{border:"2px solid #000", padding: "17.5px 60px 17.5px 17px"}}
+          className="whitespace-nowra w-full justify-center pl-[17px] pr-[60px] py-[21px] border-2 border-solid border-black align-items: start font: 400 15px Roboto, sans-serif mt-9 max-md:pr-5"
+          
           name="password"
           placeholder="Password"
           type="password"
@@ -77,20 +72,18 @@ function LoginView() {
           value={password}
         />
         <div className="flex items-stretch justify-between gap-5 mt-12 max-md:mt-10">
-          <button className="login-button max-md:px-5" onClick={onLoginClick}>
+          <button className="text-white tracking-[0.52px] whitespace-nowrap bg-[#6a7dc2] w-6/12 justify-center px-[46px] py-[22px] rounded-md border-2 border-solid border-[#6a7dc2] max-md:px-5 text-sm font-bold" onClick={onLoginClick}>
             LOGIN
           </button>
           <button
-            className="create-account-button"
-            onClick={onCreateAccountClick}
-          >
+            className="text-black tracking-[0.52px] whitespace-nowrap bg-[#c9c9c9] text-center justify-center px-5 py-2.5 rounded-md border-2 border-solid border-[#c9c9c9] text-sm font-bold"
+            onClick={onCreateAccountClick}>
             CREATE ACCOUNT
           </button>
         </div>
         <button
-          className="forgot-password-button"
-          onClick={onForgotPasswordClick}
-        >
+          className="text-black text-center tracking-[0.52px] bg-[#c9c9c9] justify-center mt-[10%] mx-[20%] p-5 rounded-md border-2 border-solid border-[#c9c9c9] text-sm font-bold"
+          onClick={onForgotPasswordClick}>
           FORGET PASSWORD
         </button>
       </div>
