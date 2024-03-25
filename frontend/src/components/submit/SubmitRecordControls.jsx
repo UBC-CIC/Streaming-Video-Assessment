20e176efd06456077f307b31704bdb1b781dcd89
hmitgang <hmitgang@gmail.com>
2024-03-25 13:44:30 -0700

    Fix toggle button not rendering properly

--- frontend/src/components/submit/SubmitRecordControls.jsx
+++ frontend/src/components/submit/SubmitRecordControls.jsx
@@ -46,16 +46,20 @@ const SubmitRecordControls = ({
         <span>{formatSeconds(secondsRemaining)}</span>
       </div>
       {allowFaceBlur && (
-        <label className="flex items-center justify-center flex-wrap p-2 cursor-pointer">
+        <div className="pl-5 flex items-center">
           <span className="mr-2 text-nowrap">Face Blur</span>
-          <input
-            type="checkbox"
-            className="toggle toggle-info"
-            disabled={!modelsLoaded}
-            onChange={(e) => setBlurface(e.target.checked)}
-            checked={blurface}
-          />
-        </label>
+          <label className="relative inline-flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              value=""
+              className="sr-only peer"
+              disabled={!modelsLoaded}
+              onChange={(e) => setBlurface(e.target.checked)}
+              checked={blurface}
+            />
+            <div className="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          </label>
+        </div>
       )}
     </div>
   );
