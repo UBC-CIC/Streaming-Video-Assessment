21e1792d70a57980e593a43bc2cd3ca00675fcab
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2024-04-04 15:19:12 -0700

    added drawer and logout button

---
+++ frontend/src/components/SideBar.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { IoLogOutOutline } from "react-icons/io5";
+
+function SideBar() {
+  const navigate = useNavigate();
+  const onLogoutClick = () => {
+    navigate("/logout");
+  };
+
+  return (
+    <ul className="menu p-6 w-80 min-h-full bg-base-200 text-base-content">
+      <li className="flex flex-row" onClick={onLogoutClick}>
+        <div className="w-full text-red-600">
+          <IoLogOutOutline size={22} />
+          <a className="text-md">Log Out</a>
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+export default SideBar;
