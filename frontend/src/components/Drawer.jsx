5439ab8dd9d13710cf4a489de202a8897f630c88
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2024-04-05 18:12:02 -0700

    made changes based on comments

--- frontend/src/components/Drawer.jsx
+++ frontend/src/components/Drawer.jsx
@@ -6,7 +6,6 @@ function Drawer({ children }) {
     <div className="drawer drawer-end">
       <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        {/* Page content here */}
         <div className="flex flex-row justify-end">
           <label
             htmlFor="my-drawer-4"
